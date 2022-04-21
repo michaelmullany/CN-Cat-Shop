1594aafaf14d96aa23bc3a7ba99a556ee0325682
Sammi Turner <ms.sammi.turner@gmail.com>
2022-04-21 17:40:21 +0100

    Add files via upload

---
+++ src/Header.js
@@ -0,0 +1,12 @@
+import './Header.css';
+
+const Header = () => {
+    return (
+        <div className='header'>
+            <h1>PawsIn</h1>
+            <p className='subtitle'>Paws in for a penny, paws in for a pound.</p>
+        </div>
+    )
+}
+
+export default Header;
